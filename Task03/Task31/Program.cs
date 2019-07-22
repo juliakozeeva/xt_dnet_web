385d713258192d382192ade9250495b61c230d34
juliakozeeva <yulya.kozeeva@mail.ru>
2019-07-22 10:03:20 +0400

    Task03 add 3.1, 3.2

---
+++ Task03/Task31/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое натуральное число: ");
+            bool input = int.TryParse(Console.ReadLine(), out int num);
+            if(input && num > 0)
+                Lost(num);
+            else
+                Console.WriteLine("Повторите ввод.");
+        }
+
+        public static void Lost (int num)
+        {
+            ArrayList array = new ArrayList(num);
+            for (int i = 1; i <= num; i++)
+            {
+                array.Add(i);
+                Console.Write(array[i - 1] + " ");
+            }
+            Console.WriteLine();
+            int index = 1;
+            while (array.Count != 1)
+            {
+                array.RemoveAt(index);
+                index++;
+
+                if (index > array.Count)
+                {
+                    index = 1;
+                }
+                if (index == array.Count)
+                {
+                    index = 0;
+                }
+                for (int i = 0; i < array.Count; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+
+}
---
+++ Task03/Task32/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task32
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст ");
+
+            string text = Console.ReadLine();
+
+            WordFrequency(text);
+
+            Console.ReadKey();
+        }
+
+        public static void WordFrequency(string text)
+        {
+
+            List<string> list = new List<string>();
+
+            String[] words = text.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                list.Add(words[i]);
+            }
+
+
+            List<string> set = new List<string>(list.Distinct());
+
+            for (int i = 0; i < set.Count; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (set[i].Equals(list[j]))
+                    {
+                        count++;
+                    }
+                }
+               Console.WriteLine("Слово {0} \tвстречается {1} раз(а)", set[i], count);
+            }
+        }
+    }
+}
