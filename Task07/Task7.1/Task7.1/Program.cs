9087cb93ab0d19e11f65112b7ae840097310c1cb
juliakozeeva <yulya.kozeeva@mail.ru>
2019-08-26 15:37:12 +0400

    add Task 7.1

---
+++ Task07/Task7.1/Task7.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text containing a date in the format dd-mm-yyyy:");
+            string inputText = Console.ReadLine();
+            if (CheckInputDateValue(inputText))
+            {
+                Console.WriteLine($"The text '{inputText}' contains the date.");
+            }
+            else
+            {
+                Console.WriteLine($"There is no date in text: '{inputText}'");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool CheckInputDateValue (string input)
+        {
+            Regex regex = new Regex(@"((0[1-9]|[1-2]\d|3[0-1])-(0[1-9]|1[0-2])-[1-2]\d{3})");
+            if (regex.IsMatch(input))
+                return true;
+            return false;
+        }
+    }
+}
