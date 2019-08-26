957c7204867ec93ec132230eeeb7e061b4ccb984
juliakozeeva <yulya.kozeeva@mail.ru>
2019-08-26 17:46:58 +0400

    add Task 7.2

---
+++ Task07/Task7.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the text:");
+            string inputText = Console.ReadLine();
+            string outputText =TextReplacement(inputText);
+            Console.WriteLine($"Result of replacement: {outputText}");
+        }
+        public static string TextReplacement (string input) => Regex.Replace(input, @"<(\/?[^>]+)>", "_");
+    }
+}
